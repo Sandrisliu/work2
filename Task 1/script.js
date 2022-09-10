c2cb55353d17d6352177f3d1ca60c489123d3b92
Sandrasliu <sandrasliuzeviciene@gmail.com>
2022-09-10 13:31:59 +0300

    first task done

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,10 +1,37 @@
-/* ------------------------------ TASK 1 ----------------------------
-Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
-pamatyti jo pateikto svorio kovertavimą į:
-1. Svarus (lb) | Formulė: lb = kg * 2.2046
-2. Gramus (g) | Formulė: g = kg / 0.0010000
-3. Uncijos (oz) | Formul4: oz = kg * 35.274
+let kgRef = document.getElementById ('kg');
+let lbRef = document.getElementById ('lb');
+let gRef = document.getElementById ('g');
+let ozRef = document.getElementById ('oz');
 
-Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
-<div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
-------------------------------------------------------------------- */
+let convertFromkg = () => {
+  let kg = kgRef.value;
+  lbRef.value = (kg * 2.2046);
+  gRef.value = (kg / 0.0010000);
+  ozRef.value = (kg * 35.274);
+}
+
+let convertFromlb = () => {
+  let lb = lbRef.value;
+  kgRef.value = (lb / 2.2046);
+  gRef.value = (lb / 0.0022046);
+  ozRef.value = (lb * 16.000);
+}
+
+let convertFromg = () => {
+  let g = gRef.value;
+  kgRef.value = (g / 1000.0);
+  lbRef.value = (g * 0.0022046);
+  ozRef.value = (g * 0.035274);
+}
+
+let convertFromoz = () => {
+  let oz = gRef.value;
+  kgRef.value = (oz / 35.274);
+  gRef.value = (oz / 0.035274);
+  lbRef.value = (oz * 0.062500);
+}
+
+kgRef.addEventListener('input', convertFromkg);
+gRef.addEventListener('input', convertFromg);
+lbRef.addEventListener('input', convertFromlb);
+ozRef.addEventListener('input', convertFromoz);
