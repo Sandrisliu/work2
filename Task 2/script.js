49f198ae263a01218011c2bb4fd4e2d5a7ac164f
Sandrasliu <sandrasliuzeviciene@gmail.com>
2022-09-10 13:37:20 +0300

    first task done

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,5 +1,9 @@
-/* ------------------------------ TASK 2 ----------------------------
-Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
-su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
-pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
-------------------------------------------------------------------- */
+var count = 0;
+      var btn = document.getElementById("btn");
+      var disp = document.getElementById("display");
+
+      btn.onclick = function () {
+          count++;
+          disp.innerHTML = count;
+      }
+
